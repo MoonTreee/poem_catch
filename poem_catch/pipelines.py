d6c30b3d82ac538431bc6e455594e3261266a295
MoonTree <297764448@qq.com>
2018-05-08 16:29:48 +0800

    爬取古诗词网的诗词

--- poem_catch/pipelines.py
+++ poem_catch/pipelines.py
@@ -4,8 +4,16 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+
+import scrapy
 
 
 class PoemCatchPipeline(object):
     def process_item(self, item, spider):
+        line = item['name'] + '\t' + item['dynasty']+ '\t' +item['writer']+ '\t' +item['content']+ '\t' +item['type']
+        line = line.replace("\n", '') + "\n"
+        file = open("poem.txt", "a+")
+        file.writelines(line)
         return item
+
--- poem_catch/spiders/poem_spider.py
+++ poem_catch/spiders/poem_spider.py
@@ -11,12 +11,32 @@ class MySpider(scrapy.Spider):
     url_head = "https://so.gushiwen.org"
 
     def start_requests(self):
-        yield Request(self.start_url, self.parse)
+        yield Request(self.start_url, self.get_url)
 
-    def parse(self, response):
-        urls = []
+    def get_url(self, response):
         soup = BeautifulSoup(response.text, "lxml").find_all("div", class_="cont")[1]
         for s in soup.find_all('a'):
-            urls.append(s['href'])
+            yield Request(self.url_head + s['href'], self.parse_url)
         # sel = Selector(response).xpath("/html/body/div[3]/div[2]/div[1]/div[2]")
-        print(urls)
+
+    def parse_url(self, response):
+        soup = BeautifulSoup(response.text, "lxml").find('div', class_="sons")
+        for s in soup.find_all("span"):
+            url =self.url_head + s.find('a')['href']
+            yield Request(url, self.parse)
+
+    def parse(self, response):
+        soup = BeautifulSoup(response.text, "lxml")
+        # name = Selector(response).select("/html/body/div[3]/div[1]/div[2]/div[1]/h1")
+        name = soup.find("h1",style="font-size:20px; line-height:22px; height:22px; margin-bottom:10px;").get_text()
+        content = soup.find("div", class_= "contson").get_text()
+        writer = soup.find('p',class_="source").find_all('a')[1].get_text()
+        dynasty = soup.find('p',class_="source").find_all('a')[0].get_text()
+        type = soup.find("div", class_="tag").get_text().replace("\n", "")
+        item = PoemCatchItem()
+        item["name"] = name
+        item['content'] = content
+        item['writer'] = writer
+        item['dynasty']= dynasty
+        item['type'] = type
+        yield item
